3638045819c31669620252425e53f1a4d4647066
kahfiaulia <62365919+kahfiaulia@users.noreply.github.com>
2024-08-16 08:00:04 +0700

    request retry session

--- fancaps-downloader.py
+++ fancaps-downloader.py
@@ -7,18 +7,22 @@ import io
 from scraper.crawler import Crawler
 from stqdm.stqdm import stqdm
 
-st.title('Fancaps Downloader')
-st.markdown(
-    f'''Thanks to m-patino for the crawling code. His repo: <a href="https://github.com/m-patino/fancaps-downloader" target="_self">fancaps-downloader by m-patino</a>''',
-    unsafe_allow_html=True
-)
-tutor = '''URL support:
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
-https://fancaps.net/{tv|anime}/showimages.php?...: Url of season page
-https://fancaps.net/{tv|anime}/episodeimages.php?...: Url of episode page
-https://fancaps.net/movies/MovieImages.php?...: Url of movie page
-'''
-st.markdown(tutor)
+def requests_retry_session(retries=3, backoff_factor=0.3, status_forcelist=(500, 502, 504)):
+    session = requests.Session()
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount("http://", adapter)
+    session.mount("https://", adapter)
+    return session
 
 @st.cache_data
 def crawl_and_download(url):
@@ -37,9 +41,14 @@ def crawl_and_download(url):
 
             for url in stqdm(links):
                 image_name = os.path.basename(url)
-                response = requests.get(url)
-                image_path = f"{subfolder}/{image_name}"
-                zipf.writestr(image_path, response.content)
+                
+                try:
+                    response = requests_retry_session().get(url, timeout=10)
+                    response.raise_for_status()
+                    image_path = f"{subfolder}/{image_name}"
+                    zipf.writestr(image_path, response.content)
+                except requests.exceptions.RequestException as e:
+                    st.warning(f"Failed to download {url}: {e}")
 
     zip_buffer.seek(0)
     return zip_buffer, main_folder
